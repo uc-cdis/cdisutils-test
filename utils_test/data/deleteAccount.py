f255bdd3e94d718238e9bde31b6ab1c37cd214bb
jesuspg <jesuspg@uchicago.edu>
2017-04-26 03:31:07 +0000

    feat(data): add test data

---
+++ utils_test/data/deleteAccount.py
@@ -0,0 +1,19 @@
+values = {
+    "password=Password123&id=72": {
+        "status_code": "200",
+        "text": {
+            "responseData": {},
+            "responseHeader": {
+                "now": 1492028881833,
+                "requestId": "WO6N0QoQgF4AAAZwzBwAAACS",
+                "status": "ok"
+            },
+            "responseStatus": "ok"
+        }
+    },
+
+    "password=Password123&id=95": {
+        "status_code": "200",
+        "text": ""
+    }
+}
