dde05fc753a3a3f17ffff04cdc7f994198fff33d
philloooo <phillis.tt@gmail.com>
2017-05-15 15:50:47 -0500

    chore(name): renmae to cdisutilstest

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="utilstest",
+    name="cdisutilstest",
     version="0.1.0",
     description="Collection of test data and tools",
     license="Apache",
