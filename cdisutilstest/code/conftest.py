86f29bc061834ee4915bb4d8feca53123b079f7e
Justin Barnowski <jbarno@uchicago.edu>
2018-03-16 16:44:06 -0500

    doc(fixture): add fixture docs. Renamed

--- cdisutilstest/code/conftest.py
+++ cdisutilstest/code/conftest.py
@@ -8,7 +8,12 @@ from indexd_fixture import (IndexClient, MockServer, create_user,
 
 
 @pytest.fixture(scope='session')
-def mock_server():
+def indexd_server():
+    """
+    Starts the indexd server, and cleans up its mess.
+    Most tests will use the client which stems from this
+    sever fixture. Runs once per test session.
+    """
     port = 8001
     indexd = Process(target=run_indexd, args=[port])
     indexd.start()
@@ -19,14 +24,14 @@ def mock_server():
     wait_for_indexd_not_alive(port)
 
 @pytest.fixture(scope='function')
-def indexd_server(mock_server):
-    # It is a misnomer this is returning a client
+def indexd_client(indexd_server):
+    """
+    Returns a IndexClient. This will delete any documents
+    made by this client after the test has completed.
+    Runs once per test.
+    """
     client = IndexClient(
-        baseurl=mock_server.baseurl, auth=mock_server.auth)
+        baseurl=indexd_server.baseurl, auth=indexd_server.auth)
     yield client
     for doc in client.list():
         doc.delete()
-
-@pytest.fixture(scope='function')
-def indexd_client(indexd_server):
-    return indexd_server
