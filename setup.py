e8a0fe23b7f63811755bb950af3317547cd7be9b
Rowland Ogwara <rogwara@uchicago.edu>
2018-05-10 13:11:49 -0500

    version 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cdisutilstest",
-    version="0.2.1",
+    version="0.2.3",
     description="Collection of test data and tools",
     license="Apache",
     packages=find_packages(),
