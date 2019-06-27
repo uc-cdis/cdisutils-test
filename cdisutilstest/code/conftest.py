d29c23ae78d8a9c58ca2ffa978b0950910a0163f
Pauline Ribeyre <ribeyre@uchicago.edu>
2019-06-27 16:57:38 -0500

    break it

--- cdisutilstest/code/conftest.py
+++ cdisutilstest/code/conftest.py
@@ -2,10 +2,7 @@ from multiprocessing import Process
 
 import pytest
 
-from .indexd_fixture import (IndexClient, MockServer, create_user,
-                            remove_sqlite_files, run_indexd,
-                            clear_database, setup_database,
-                            wait_for_indexd_alive, wait_for_indexd_not_alive)
+from .indexd_fixture import (IndexClient, MockServer, create_user, remove_sqlite_files, run_indexd, clear_database, setup_database, wait_for_indexd_alive, wait_for_indexd_not_alive)
 # Note the . in front of indexd_fixtures for more information:
 # https://stackoverflow.com/questions/16981921/relative-imports-in-python-3#16985066
 # Basically the options are:
